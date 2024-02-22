cf91a91c036360d3926a4f767d6b862228170772
manuelloverso <manuelloverso@gmail.com>
2024-02-22 16:02:30 +0100

    add mail validation

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+//Variables declaration
+const userMail = "geppino@gmail.com";
+const mailArray = [
+  "giuseppino@gmail.com",
+  "pippo@libero.it",
+  "pluto@gmail.com",
+  "topolino@gmail.com",
+];
+console.log(mailArray);
+
+//Loop to scroll through mails
+for (let i = 0; i < mailArray.length; i++) {
+  const mailIndex = mailArray[i];
+  if (userMail == mailIndex) {
+    console.log("Puoi accedere");
+  } else {
+    console.log("La tua mail non è stata trovata, riprova");
+  }
+}
